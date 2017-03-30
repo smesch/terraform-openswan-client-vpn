19dfaea6d8f4382fd20c35d656781c3dbe1ad8f6
ryane <ryanesc@gmail.com>
2017-03-30 17:20:00 -0400

    rename outputs (so they are easier to consume from other modules)

--- outputs.tf
+++ outputs.tf
@@ -1,12 +1,15 @@
-output "1. Openswan VPN Server IP" {
-    value = "${aws_eip.openswan-server.public_ip}"
+output "openswan_vpn_server_ip" {
+  value = "${aws_eip.openswan-server.public_ip}"
 }
-output "2. Openswan VPN Client Username" {
-    value = "${var.vpn_username}"
+
+output "openswan_vpn_client_username" {
+  value = "${var.vpn_username}"
 }
-output "3. Openswan VPN Client Password" {
-    value = "${var.vpn_password}"
+
+output "openswan_vpn_client_password" {
+  value = "${var.vpn_password}"
 }
-output "4. Openswan VPN Client Pre-Shared Key" {
-    value = "${var.vpn_preshared_key}"
+
+output "openswan_vpn_client_pre_shared_key" {
+  value = "${var.vpn_preshared_key}"
 }
